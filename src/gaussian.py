6a0719b96fd20231b1e85e2fbf55c148d9c5577e
amartinsmg <andremindurimg@gmail.com>
2022-08-29 11:51:18 -0300

    Update gaussian.py

--- src/gaussian.py
+++ src/gaussian.py
@@ -5,11 +5,11 @@ from math import exp, pi, sqrt
 def gaussianCDF(x, mean, standadDev):
     denominator = 1
     sum = 0
-    assert (standadDev > 0)
+    assert standadDev > 0
     z = (x - mean) / standadDev
-    phi = exp(-pow(z, 2) / 2) / sqrt(2 * pi)
+    phi = exp(-(z ** 2) / 2) / sqrt(2 * pi)
     for i in range(1, 100, 2):
         denominator *= i
-        sum += pow(z, i) / denominator
+        sum += z ** i / denominator
     result = 0.5 + phi * sum
     return result
