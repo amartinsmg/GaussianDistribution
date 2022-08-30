e9640208adb7888150dbbf10f5c37473391facae
amartinsmg <andremindurimg@gmail.com>
2022-08-30 11:57:53 -0300

    Add gaussian.cs

---
+++ src/gaussian.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics;
+
+namespace Calc
+{
+  class Gasussian
+  {
+    static double GasussianCDF(double x, double mean, double standardDev)
+    {
+      double phi, result, z, denominator = 1,
+        sum = 0;
+      int i;
+      Debug.Assert(standardDev > 0);
+      z = (x - mean) / standardDev;
+      phi = Math.Exp(-Math.Pow(z, 2) / 2) / Math.Sqrt(2 * Math.PI);
+      for (i = 1; i <= 100; i += 2)
+      {
+        denominator *= i;
+        sum += Math.Pow(z, i) / denominator;
+      }
+      result = 0.5 + phi * sum;
+      return result;
+    }
+  }
+}
