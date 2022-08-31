ced4fa43cd70bcdbe826bdc635366601efe93784
amartinsmg <andremindurimg@gmail.com>
2022-08-30 22:08:48 -0300

    Update python

--- src/py/gaussian.py
+++ src/py/gaussian.py
@@ -3,13 +3,13 @@ from math import exp, pi, sqrt
 
 
 def gaussianCDF(mean, standardDev, x):
-    denominator = 1
-    sum = 0
-    assert standardDev > 0
-    z = (x - mean) / standardDev
-    phi = exp(-(z ** 2) / 2) / sqrt(2 * pi)
-    for i in range(1, 100, 2):
-        denominator *= i
-        sum += z ** i / denominator
-    result = 0.5 + phi * sum
-    return result
+	denominator = 1
+	sum = 0
+	assert standardDev > 0
+	z = (x - mean) / standardDev
+	phi = exp(-(z ** 2) / 2) / sqrt(2 * pi)
+	for i in range(1, 100, 2):
+		denominator *= i
+		sum += z ** i / denominator
+	result = 0.5 + phi * sum
+	return result
--- src/py/sqlite_block.py
+++ src/py/sqlite_block.py
@@ -1,21 +1,19 @@
 from gaussian import gaussianCDF
+import os
 import sqlite3
-from os import path
 
 if __name__ == '__main__':
-    databasePath = path.abspath(path.dirname(
-        __file__) + '/../../sqlite/database.db')
+    databasePath = os.path.dirname(__file__) + '/../../sqlite/database.db'
     query = ''
     db = sqlite3.connect(databasePath)
-    cur = db.cursor()
-    cur.executescript('''DROP TABLE IF EXISTS py_block; CREATE TABLE py_block(id INTEGER PRIMARY KEY
-    AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
+    db.executescript('''DROP TABLE IF EXISTS py_block; CREATE TABLE py_block(id INTEGER PRIMARY KEY
+	AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
 
     for i in range(-500, 501):
         x = i / 100
         cumulativeD = gaussianCDF(0, 1, x)
         query += f'INSERT INTO py_block(z_score, cumulative_distribution) VALUES ({x:.2f}, {cumulativeD:.6f});'
 
-    cur.executescript(query)
+    db.executescript(query)
     db.commit()
     db.close()
