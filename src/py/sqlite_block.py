fae591909b252d5b97c45398ac61078879bf3107
amartinsmg <andremindurimg@gmail.com>
2022-09-01 19:21:46 -0300

    Update sqlite_block.py and sqlite_serial.py

--- src/py/sqlite_block.py
+++ src/py/sqlite_block.py
@@ -2,18 +2,17 @@ from gaussian import gaussianCDF
 import os
 import sqlite3
 
-if __name__ == '__main__':
-	databasePath = os.path.dirname(__file__) + '/../../sqlite/database.db'
-	query = ''
-	db = sqlite3.connect(databasePath)
-	db.executescript('''DROP TABLE IF EXISTS py_block; CREATE TABLE py_block(id INTEGER PRIMARY KEY
-	AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
+databasePath = os.path.dirname(__file__) + '/../../sqlite/database.db'
+query = ''
+db = sqlite3.connect(databasePath)
+db.executescript('''DROP TABLE IF EXISTS py_block; CREATE TABLE py_block(id INTEGER PRIMARY KEY
+AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
 
-	for i in range(-500, 501):
-		x = i / 100
-		cumulativeD = gaussianCDF(0, 1, x)
-		query += f'INSERT INTO py_block(z_score, cumulative_distribution) VALUES ({x:.2f}, {cumulativeD:.6f});'
+for i in range(-500, 501):
+	x = i / 100
+	cumulativeD = gaussianCDF(0, 1, x)
+	query += f'INSERT INTO py_block(z_score, cumulative_distribution) VALUES ({x:.2f}, {cumulativeD:.6f});'
 
-	db.executescript(query)
-	db.commit()
-	db.close()
+db.executescript(query)
+db.commit()
+db.close()
--- src/py/sqlite_serial.py
+++ src/py/sqlite_serial.py
@@ -2,17 +2,16 @@ from gaussian import gaussianCDF
 import os
 import sqlite3
 
-if __name__ == '__main__':
-	databasePath = os.path.dirname(__file__) + '/../../sqlite/database.db'
-	db = sqlite3.connect(databasePath)
-	db.executescript('''DROP TABLE IF EXISTS py_serial; CREATE TABLE py_serial(id INTEGER PRIMARY KEY
-	AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
+databasePath = os.path.dirname(__file__) + '/../../sqlite/database.db'
+db = sqlite3.connect(databasePath)
+db.executescript('''DROP TABLE IF EXISTS py_serial; CREATE TABLE py_serial(id INTEGER PRIMARY KEY
+AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
 
-	for i in range(-500, 501):
-		x = i / 100
-		cumulativeD = gaussianCDF(0, 1, x)
-		db.execute(
-			f'INSERT INTO py_serial(z_score, cumulative_distribution) VALUES ({x:.2f}, {cumulativeD:.6f})')
+for i in range(-500, 501):
+	x = i / 100
+	cumulativeD = gaussianCDF(0, 1, x)
+	db.execute(
+		f'INSERT INTO py_serial(z_score, cumulative_distribution) VALUES ({x:.2f}, {cumulativeD:.6f})')
 
-	db.commit()
-	db.close()
+db.commit()
+db.close()
