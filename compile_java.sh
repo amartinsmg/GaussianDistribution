c951c8a6f117fbb67baf51ca1d8a809b7616e7c2
amartinsmg <andremindurimg@gmail.com>
2022-09-05 18:09:22 -0300

    Update compile_java.sh

--- compile_java.sh
+++ compile_java.sh
@@ -8,6 +8,7 @@ JAVAFILE=$(echo $2 | sed 's/src\/java\///g')
 CLASS=$(echo $JAVAFILE | sed 's/\.java//g')
 OUTPUT="$(echo $CLASS | tr '[:upper:]' '[:lower:]').jar"
 SQLITE_JDBC="$LIBS_DIR/sqlite-jdbc-3.36.0.3.jar"
+MYSQL_JDBC="C:/Program Files (x86)/MySQL/Connector J 8.0/mysql-connector-java-8.0.30.jar"
 
 mkdir $TMP_DIR
 cd $SRC_DIR
@@ -22,9 +23,16 @@ case $1 in
       cd $LIBS_DIR
       curl -LO https://github.com/xerial/sqlite-jdbc/releases/download/3.36.0.3/sqlite-jdbc-3.36.0.3.jar
     fi
-
     cd $TMP_DIR
     unzip -uo $SQLITE_JDBC
+    cd $BUILD_DIR
+    jar -cfe $OUTPUT $CLASS -C $TMP_DIR .
+    rm -rf $TMP_DIR
+  ;;
+
+  "--mysql")
+    cd $TMP_DIR
+    unzip -uo "$MYSQL_JDBC"
     cd $BUILD_DIR
     jar -cfe $OUTPUT $CLASS -C $TMP_DIR .
     rm -rf $TMP_DIR
