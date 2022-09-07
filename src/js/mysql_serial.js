2f4187c4b1b917db4ed328d24237ebefb95b2b5d
amartinsmg <andremindurimg@gmail.com>
2022-09-06 22:35:09 -0300

    Update js_mysql

--- src/js/mysql_serial.js
+++ src/js/mysql_serial.js
@@ -1,35 +1,29 @@
 const { gaussianCDF } = require("./gaussian"),
-  mysql = require("mysql2"),
-  callback = function (err) {
-    if (err) console.log(err.message);
-  };
+  mysql = require("mysql2");
 
-let conn = mysql.createConnection({
+try {
+  let conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root123",
     database: "gaussian",
     flags: "MULTI_STATEMENTS",
   });
-
-conn.connect(callback);
-
-conn.query(
-  "DROP TABLE IF EXISTS js_serial; CREATE TABLE js_serial(id INTEGER AUTO_INCREMENT " +
-    "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);",
-  callback
-);
-
-for (let i = -500; i <= 500; i++) {
-  let x = i / 100,
-    prob = gaussianCDF(0, 1, x);
+  conn.connect();
   conn.query(
-    `INSERT INTO js_serial(z_score, cumulative_distribution) VALUES (${x}, ${prob.toFixed(
-      6
-    )});`,
-    callback
+    "DROP TABLE IF EXISTS js_serial; CREATE TABLE js_serial(id INTEGER AUTO_INCREMENT " +
+      "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);"
   );
+  for (let i = -500; i <= 500; i++) {
+    let x = i / 100,
+      prob = gaussianCDF(0, 1, x);
+    conn.query(
+      `INSERT INTO js_serial(z_score, cumulative_distribution) VALUES (${x}, ${prob.toFixed(
+        6
+      )});`
+    );
+  }
+  conn.end();
+} catch (err) {
+  console.log(err.message);
 }
-
-conn.end();
-
