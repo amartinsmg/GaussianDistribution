d3d2a0884e204a2d44f0c10fb054b9ef809e6544
amartinsmg <andremindurimg@gmail.com>
2022-09-06 22:35:26 -0300

    Update js_sqlite

--- src/js/sqlite_block.js
+++ src/js/sqlite_block.js
@@ -1,27 +1,24 @@
 const { gaussianCDF } = require("./gaussian"),
-  sqlite3 = require("sqlite3").verbose(),
-  callback = function (err) {
-    if (err) console.log(err.message);
-  };
+  sqlite3 = require("sqlite3").verbose();
 
-let query = "",
-  dbPath = __dirname + "/../../sqlite/database.db",
-  conn = new sqlite3.Database(dbPath, callback);
-
-conn.exec(
-  "DROP TABLE IF EXISTS js_block; CREATE TABLE js_block(id INTEGER PRIMARY KEY " +
-    "AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);",
-  callback
-);
-
-for (let i = -500; i <= 500; i++) {
-  let x = i / 100,
-    prob = gaussianCDF(0, 1, x);
-  query += `INSERT INTO js_block(z_score, cumulative_distribution) VALUES (${x}, ${prob.toFixed(
-    6
-  )});`;
+try {
+  let query = "",
+    dbPath = __dirname + "/../../sqlite/database.db",
+    conn = new sqlite3.Database(dbPath);
+  conn.exec(
+    "DROP TABLE IF EXISTS js_block; CREATE TABLE js_block(id INTEGER PRIMARY KEY " +
+      "AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);",
+    callback
+  );
+  for (let i = -500; i <= 500; i++) {
+    let x = i / 100,
+      prob = gaussianCDF(0, 1, x);
+    query += `INSERT INTO js_block(z_score, cumulative_distribution) VALUES (${x}, ${prob.toFixed(
+      6
+    )});`;
+  }
+  conn.exec(query);
+  conn.close();
+} catch (err) {
+  console.log(err.message);
 }
-
-conn.exec(query, callback);
-
-conn.close();
--- src/js/sqlite_serial.js
+++ src/js/sqlite_serial.js
@@ -1,27 +1,23 @@
 const { gaussianCDF } = require("./gaussian"),
-  sqlite3 = require("sqlite3").verbose(),
-  callback = function (err) {
-    if (err) console.log(err.message);
-  };
+  sqlite3 = require("sqlite3").verbose();
 
-let dbPath = __dirname + "/../../sqlite/database.db",
-  conn = new sqlite3.Database(dbPath, callback);
-
-conn.exec(
-  "DROP TABLE IF EXISTS js_serial; CREATE TABLE js_serial(id INTEGER PRIMARY KEY " +
-    "AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);",
-  callback
-);
-
-for (let i = -500; i <= 500; i++) {
-  let x = i / 100,
-    prob = gaussianCDF(0, 1, x);
+try {
+  let dbPath = __dirname + "/../../sqlite/database.db",
+    conn = new sqlite3.Database(dbPath);
   conn.exec(
-    `INSERT INTO js_serial(z_score, cumulative_distribution) VALUES (${x}, ${prob.toFixed(
-      6
-    )});`,
-    callback
+    "DROP TABLE IF EXISTS js_serial; CREATE TABLE js_serial(id INTEGER PRIMARY KEY " +
+      "AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);"
   );
+  for (let i = -500; i <= 500; i++) {
+    let x = i / 100,
+      prob = gaussianCDF(0, 1, x);
+    conn.exec(
+      `INSERT INTO js_serial(z_score, cumulative_distribution) VALUES (${x}, ${prob.toFixed(
+        6
+      )});`
+    );
+  }
+  conn.close();
+} catch (err) {
+  console.log(err.message);
 }
-
-conn.close();
