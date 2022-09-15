cd70826b497b72565939856e87962be9f6b9f250
amartinsmg <andremindurimg@gmail.com>
2022-09-15 12:00:11 -0300

    Update py_sqlite

--- src/py/sqlite_block.py
+++ src/py/sqlite_block.py
@@ -10,11 +10,13 @@ try:
 	cur = conn.cursor()
 	cur.executescript('''DROP TABLE IF EXISTS tb_py_block; CREATE TABLE tb_py_block(id INTEGER PRIMARY KEY
 	AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
+	conn.commit()
 	for i in range(-500, 501):
 		x = i / 100
 		prob = gaussianCDF(0, 1, x)
 		query += f'INSERT INTO tb_py_block(z_score, cumulative_distribution) VALUES ({x:.2f}, {prob:.6f});'
 	cur.executescript(query)
+	conn.commit()
 	conn.close()
 except Exception as e:
 	print(e)
