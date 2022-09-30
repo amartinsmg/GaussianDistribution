c643bc845fb617689e02ac2b5cd26c11df56d4f8
amartinsmg <andremindurimg@gmail.com>
2022-09-30 11:50:02 -0300

    Update py_postgres

--- src/py/postgres_block.py
+++ src/py/postgres_block.py
@@ -9,16 +9,15 @@ try:
 		password="root123",
 		database="gaussian",
 	)
+	conn.autocommit = True
 	cur = conn.cursor()
 	cur.execute('''DROP TABLE IF EXISTS tb_py_block; CREATE TABLE tb_py_block(id SERIAL PRIMARY KEY,
 	z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
-	conn.commit()
 	for i in range(-500, 501):
 		x = i / 100
 		prob = gaussianCDF(0, 1, x)
 		query += f'INSERT INTO tb_py_block(z_score, cumulative_distribution) VALUES ({x:.2f}, {prob:.6f});'
 	cur.execute(query)
-	conn.commit()
 	conn.close()
 except Exception as e:
 	print(e)
--- src/py/postgres_serial.py
+++ src/py/postgres_serial.py
@@ -8,16 +8,15 @@ try:
     password="root123",
     database="gaussian",
   )
+  conn.autocommit = True
   cur = conn.cursor()
   cur.execute('''DROP TABLE IF EXISTS tb_py_serial; CREATE TABLE tb_py_serial(id SERIAL PRIMARY KEY,
   z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
-  conn.commit()
   for i in range(-500, 501):
     x = i / 100
     prob = gaussianCDF(0, 1, x)
     cur.execute(
       f'INSERT INTO tb_py_serial(z_score, cumulative_distribution) VALUES ({x:.2f}, {prob:.6f})')
-    conn.commit()
   conn.close()
 except Exception as e:
 	print(e)
