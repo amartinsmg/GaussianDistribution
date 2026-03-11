cc326c4e119d7d10d407c66ddc8d296e90d06b5f
amartinsmg <andremindurimg@gmail.com>
2026-03-11 17:58:36 -0300

    refactor(postgres-serial): store hashes instead of Gaussian CDF
    - Switched from Gaussian CDF to hash32 calc
    - Updated table schema to store only hashes
    - Implemented try-with-resources

--- implementations/java/src/Postgres_serial.java
+++ implementations/java/src/Postgres_serial.java
@@ -1,27 +1,24 @@
+
 import java.sql.*;
-import java.util.Locale;
 
 public class Postgres_serial {
-  public static void main(String[] args) {
-    Connection conn;
-    Statement stmt;
-    String query = "";
-    int i;
-    double x, prob;
-    Locale.setDefault(new Locale("en", "US"));
-    try {
-      conn = DriverManager.getConnection("jdbc:postgresql://localhost/gaussian", "root", "root123");
-      stmt = conn.createStatement();
-      stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial; CREATE TABLE tb_java_serial(id SERIAL " +
-      "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
-      for (i = -500; i <= 500; i++) {
-        x = (double) i / 100.0;
-        prob = Gaussian.gaussianCDF(0, 1, x);
-        query = String.format("INSERT INTO tb_java_serial(z_score, cumulative_distribution) VALUES (%.2f, %f);", x, prob);
-        stmt.executeUpdate(query);
-      }
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
+
+    public static void main(String[] args) {
+        String query;
+        int i, hash;
+        try (
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/hashes", "root", "root123");
+            Statement stmt = conn.createStatement();
+        ) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial; CREATE TABLE tb_java_serial(id SERIAL "
+                    + "PRIMARY KEY, hash INTEGER NOT NULL)");
+            for (i = 0; i <= 1000; i++) {
+                hash = Hash.hash32(i);
+                query = String.format("INSERT INTO tb_java_serial(hash) VALUES (%d)", hash);
+                stmt.executeUpdate(query);
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
-  }
 }
