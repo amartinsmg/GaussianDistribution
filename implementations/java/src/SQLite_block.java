e18ca1c2584e0dcb1490429c5eb560e68c33e8cb
amartinsmg <andremindurimg@gmail.com>
2026-03-11 17:33:20 -0300

    feat(sqlite-block): populate table with hash values
    - Generate and store hash values
    - Update DB schema to store hashes
    - Use try-with-resources for DB connection
    - Simplify database path construction
    - Remove unused Locale import

--- implementations/java/src/SQLite_block.java
+++ implementations/java/src/SQLite_block.java
@@ -1,31 +1,26 @@
+
 import java.io.File;
 import java.sql.*;
-import java.util.Locale;
 
 public class SQLite_block {
-  public static void main(String[] args) {
-    Connection conn;
-    Statement stmt;
-    String query = "",
-        dbPath = String.format("jdbc:sqlite:%s/../sqlite/database.db",
-            new File(SQLite_serial.class.getProtectionDomain().getCodeSource().getLocation().getPath().toString())
-                .getParent());
-    int i;
-    double x, prob;
-    Locale.setDefault(new Locale("en", "US"));
-    try {
-      conn = DriverManager.getConnection(dbPath);
-      stmt = conn.createStatement();
-      stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_block; CREATE TABLE tb_java_block(id INTEGER PRIMARY KEY " +
-          "AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
-      for (i = -500; i <= 500; i++) {
-        x = (double) i / 100.0;
-        prob = Gaussian.gaussianCDF(0, 1, x);
-        query += String.format("INSERT INTO tb_java_block(z_score, cumulative_distribution) VALUES (%.2f, %f);", x, prob);
-      }
-      stmt.executeUpdate(query);
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
+
+    public static void main(String[] args) {
+        String dbPath = String.format("jdbc:sqlite:%s/database.db",
+                new File(SQLite_block.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+                        .getParent());
+        String query = "INSERT INTO tb_java_block(hash) VALUES ";
+        int i, hash;
+        try (Connection conn = DriverManager.getConnection(dbPath); Statement stmt = conn.createStatement();) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_block; CREATE TABLE tb_java_block(id INTEGER PRIMARY KEY "
+                    + "AUTOINCREMENT, hash INTEGER NOT NULL)");
+            for (i = 0; i <= 1000; i++) {
+                hash = Hash.hash32(i);
+                query += String.format("(%d),", hash);
+            }
+            query = query.replaceAll(".$", ";");
+            stmt.executeUpdate(query);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
-  }
 }
