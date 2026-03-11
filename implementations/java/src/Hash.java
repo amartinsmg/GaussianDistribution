8bb9adae828af94934405136ac5562bd9fcb06cd
amartinsmg <andremindurimg@gmail.com>
2026-03-11 17:32:22 -0300

    feat(java): add hash32 utility
    - Implements a 32-bit integer hash
    - New utility class `Hash.java`

---
+++ implementations/java/src/Hash.java
@@ -0,0 +1,10 @@
+public class Hash {
+  public static int hash32(int x) {
+    x ^= x >> 16;
+    x *= 0x7feb352d;
+    x ^= x >> 15;
+    x *= 0x846ca68d;
+    x ^= x >> 16;
+    return x & 0xffffffff;
+  }
+}
