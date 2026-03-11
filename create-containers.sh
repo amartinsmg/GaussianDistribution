53a217d8334ec39535bb9c33d37aa9636b8faa0b
amartinsmg <andremindurimg@gmail.com>
2026-03-11 20:05:00 -0300

    Refactor create_db.sh as create-containers.sh

--- create-containers.sh
+++ create-containers.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-SQLITE_DIR="`pwd`/sqlite"
 POSTGRES_DIR="`pwd`/postgres"
 MYSQL_DIR="`pwd`/mysql"
 MARIADB_DIR="`pwd`/mariadb"
-
-[ -d "$SQLITE_DIR" ] || mkdir "$SQLITE_DIR"
-cd "$SQLITE_DIR"
-sqlite3 database.db .databases
 
 cd "$POSTGRES_DIR"
 docker compose create
