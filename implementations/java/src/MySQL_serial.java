0ea99f7a5db2cf7afd4046f6e91c27344d956526
amartinsmg <andremindurimg@gmail.com>
2026-03-11 17:57:17 -0300

    feat(mysql-serial): implement hash calculation
    - Switched data generation to 32-bit hashes
    - Updated MySQL table schema for hashes
    - Changed database connection string

--- implementations/java/src/MySQL_serial.java
+++ implementations/java/src/MySQL_serial.java
@@ -1,27 +1,24 @@
+
 import java.sql.*;
-import java.util.Locale;
 
 public class MySQL_serial {
-  public static void main(String[] args) {
-    Connection conn;
-    Statement stmt;
-    String query = "";
-    int i;
-    double x, prob;
-    Locale.setDefault(new Locale("en", "US"));
-    try {
-      conn = DriverManager.getConnection("jdbc:mysql://localhost/gaussian?allowMultiQueries=true", "root", "root123");
-      stmt = conn.createStatement();
-      stmt.executeUpdate("DROP TABLE IF EXISTS tb_tb_java_va_serial; CREATE TABLE tb_java__java_serial(id INTEGER AUTO_INCREMENT " +
-      "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
-      for (i = -500; i <= 500; i++) {
-        x = (double) i / 100.0;
-        prob = Gaussian.gaussianCDF(0, 1, x);
-        query = String.format("INSERT INTO tb_tb_java_va_serial(z_score, cumulative_distribution) VALUES (%.2f, %f);", x, prob);
-        stmt.executeUpdate(query);
-      }
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
+
+    public static void main(String[] args) {
+        String query;
+        int i, hash;
+        try (
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hashes?allowMultiQueries=true", "root", "root123");
+            Statement stmt = conn.createStatement();
+        ) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial; CREATE TABLE tb_java_serial(id INTEGER AUTO_INCREMENT "
+                    + "PRIMARY KEY, hash INTEGER NOT NULL)");
+            for (i = 0; i <= 1000; i++) {
+                hash = Hash.hash32(i);
+                query = String.format("INSERT INTO tb_java_serial(hash) VALUES (%d)", hash);
+                stmt.executeUpdate(query);
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
-  }
 }
