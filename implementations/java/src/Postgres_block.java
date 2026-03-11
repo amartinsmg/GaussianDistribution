ff1672c9cd5e734b982d119a2690ed1a0abc8392
amartinsmg <andremindurimg@gmail.com>
2026-03-11 17:57:57 -0300

    feat(Postgres_block): implement hash generation
    - Switched from Gaussian CDF to hash generation
    - Updated database and table schema
    - Improved resource management with try-with-resources

--- implementations/java/src/Postgres_block.java
+++ implementations/java/src/Postgres_block.java
@@ -1,28 +1,26 @@
+
 import java.sql.*;
-import java.util.Locale;
 
 public class Postgres_block {
-  public static void main(String[] args) {
-    Connection conn;
-    Statement stmt;
-    String query = "";
-    int i;
-    double x, prob;
-    Locale.setDefault(new Locale("en", "US"));
-    try {
-      conn = DriverManager.getConnection("jdbc:postgresql://localhost/gaussian", "root", "root123");
-      stmt = conn.createStatement();
-      stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_block; CREATE TABLE tb_java_block(id SERIAL " +
-          "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
-      for (i = -500; i <= 500; i++) {
-        x = (double) i / 100.0;
-        prob = Gaussian.gaussianCDF(0, 1, x);
-        query += String.format("INSERT INTO tb_java_block(z_score, cumulative_distribution) VALUES (%.2f, %f);", x, prob);
-      }
-      stmt.executeUpdate(query);
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
+
+    public static void main(String[] args) {
+        String query = "INSERT INTO tb_java_block(hash) VALUES ";
+        int i, hash;
+        try (
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/hashes", "root", "root123");
+            Statement stmt = conn.createStatement();
+        ) {
+            stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_block; CREATE TABLE tb_java_block(id SERIAL "
+                    + "PRIMARY KEY, hash INTEGER NOT NULL)");
+            for (i = 0; i <= 1000; i++) {
+                hash = Hash.hash32(i);
+                query += String.format("(%d),", hash);
+            }
+            query = query.replaceAll(".$", ";");
+            stmt.executeUpdate(query);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
-  }
 
 }
