d20811d0b3d734b5a49224ca341c97f0401fa0df
amartinsmg <andremindurimg@gmail.com>
2026-03-11 23:33:35 -0300

    feat(postgres): add bulk insert implementation
    - Implements bulk hash insertion
    - Utilizes libpq for PostgreSQL
    - Creates tb_java_bulk table
    - Inserts 10,000 generated hashes
    - Uses a single INSERT statement

---
+++ implementations/java/src/PostgresBulk.java
@@ -0,0 +1,40 @@
+
+import java.sql.*;
+
+public class PostgresBulk {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("INSERT INTO tb_java_bulk(hash) VALUES ");
+        String query;
+        int i, hash;
+        try (
+			Connection conn = DriverManager.getConnection(
+					"jdbc:postgresql://127.0.0.1:5432/hashes",
+					"root",
+					"root123"
+			);
+			Statement stmt = conn.createStatement();
+		) {
+		stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_bulk;"
+						+ "CREATE TABLE tb_java_bulk"
+						+ "(id SERIAL PRIMARY KEY,"
+						+ " hash INTEGER NOT NULL)");
+
+			conn.setAutoCommit(false);
+
+			for (i = 1; i <= 10000; i++) {
+				hash = Hash.hash32(i);
+				sb.append(String.format("(%d),", hash));
+			}
+
+			query = sb.toString().replaceAll(".$", ";");
+
+			stmt.execute(query);
+
+			conn.commit();
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+    }
+}
