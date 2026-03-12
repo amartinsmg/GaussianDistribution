5c1029cbca7b153c8730aceadc1b8cdce7758983
amartinsmg <andremindurimg@gmail.com>
2026-03-11 22:16:33 -0300

    feat(postgres): Implement hash generation
    - Replaced Gaussian CDF with hash generation
    - Updated database connection and table schema

--- implementations/c/src/postgres_serial.c
+++ implementations/c/src/postgres_serial.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <libpq-fe.h>
-#include "gaussian.h"
+#include <hash.h>
+
+#define N 10000
 
 int main(int argc, char **argv)
 {
-  PGconn *conn = PQconnectdb("user=root password=root123 dbname=gaussian");
+  PGconn *conn = PQconnectdb("host=127.0.0.1 user=root password=root123 dbname=hashes");
   PGresult *res = NULL;
-  int i;
+  int32_t i, hash;
   int exitCode = -1;
-  double x, prob;
   char query[90];
+
   if (PQstatus(conn) == CONNECTION_BAD)
   {
     fprintf(stderr, "Connection to database failed: %s\n",
             PQerrorMessage(conn));
     goto cleanup;
   }
-  res = PQexec(conn, "DROP TABLE IF EXISTS tb_c_serial; CREATE TABLE tb_c_serial(id SERIAL PRIMARY KEY, "
-                     "z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
+
+  res = PQexec(conn, "DROP TABLE IF EXISTS tb_c_serial; CREATE TABLE tb_c_serial(id "
+                     "SERIAL PRIMARY KEY, hash INTEGER NOT NULL)");
   if (PQresultStatus(res) != PGRES_COMMAND_OK)
   {
     fprintf(stderr, "%s\n", PQerrorMessage(conn));
@@ -26,11 +30,12 @@ int main(int argc, char **argv)
   }
   PQclear(res);
   res = NULL;
-  for (i = -500; i <= 500; i++)
+
+  for (i = 1; i <= N; i++)
   {
-    x = (double)i / 100.0;
-    prob = gaussianCDF(0, 1, x);
-    sprintf(query, "INSERT INTO tb_c_serial(z_score, cumulative_distribution) VALUES (%.2f, %f)", x, prob);
+    hash = hash32(i);
+    snprintf(query, 90, "INSERT INTO tb_c_serial(hash) VALUES (%d)", hash);
+
     res = PQexec(conn, query);
     if (PQresultStatus(res) != PGRES_COMMAND_OK)
     {
@@ -40,11 +45,14 @@ int main(int argc, char **argv)
     PQclear(res);
     res = NULL;
   }
+
   exitCode = 0;
 
 cleanup:
-  if (res != NULL)
+  if (res)
     PQclear(res);
-  PQfinish(conn);
+  if (conn)
+    PQfinish(conn);
+
   return exitCode;
 }
