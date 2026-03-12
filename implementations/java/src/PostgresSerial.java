0ce4666be1031371d0f422a5bf5be2a5d61bd0eb
amartinsmg <andremindurimg@gmail.com>
2026-03-11 23:36:34 -0300

    feat(postgres): increase hash insertion count
    - Changed loop to insert 10000 hashes
    - Updated table creation logic for clarity
    - Increased hash insertion range

--- implementations/java/src/PostgresSerial.java
+++ implementations/java/src/PostgresSerial.java
@@ -7,16 +7,24 @@ public class PostgresSerial {
         String query;
         int i, hash;
         try (
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/hashes", "root", "root123");
+            Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/hashes",
+                "root",
+                "root123"
+                );
             Statement stmt = conn.createStatement();
         ) {
-            stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial; CREATE TABLE tb_java_serial(id SERIAL "
-                    + "PRIMARY KEY, hash INTEGER NOT NULL)");
-            for (i = 0; i <= 1000; i++) {
-                hash = Hash.hash32(i);
-                query = String.format("INSERT INTO tb_java_serial(hash) VALUES (%d)", hash);
-                stmt.executeUpdate(query);
-            }
+			stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial;"
+					+ "CREATE TABLE tb_java_serial "
+					+ "(id SERIAL PRIMARY KEY, "
+					+ "hash INTEGER NOT NULL)");
+
+			for (i = 1; i <= 10000; i++) {
+				hash = Hash.hash32(i);
+				query = String.format("INSERT INTO tb_java_serial(hash) VALUES (%d)", hash);
+				stmt.executeUpdate(query);
+			}
+
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
