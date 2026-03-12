7959e891588cfa74136dd88f92a09dcb24ffbab5
amartinsmg <andremindurimg@gmail.com>
2026-03-12 12:15:38 -0300

    feat(mysql): add bulk insert implementation
    - Implements bulk hash insertion
    - Creates tb_c_bulk table
    - Inserts 10,000 generated hashes
    - Uses a single INSERT statement

---
+++ implementations/c/src/mysql-bulk.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <mysql.h>
+#include <hash.h>
+
+#define N 10000
+#define QUERY_SIZE 160000
+
+void clear_mysql_results(MYSQL *conn)
+{
+  MYSQL_RES *res;
+  while (mysql_next_result(conn) == 0)
+  {
+    res = mysql_store_result(conn);
+    if (res)
+      mysql_free_result(res);
+  }
+}
+
+int main(int argc, char **argv)
+{
+  MYSQL *conn = mysql_init(NULL);
+  int32_t i, hash;
+  int exitCode = -1,
+      buffer = 0;
+  double x, prob;
+  char *query = malloc(QUERY_SIZE * sizeof(*query));
+
+  if (!conn)
+  {
+    fprintf(stderr, "Failed to initialize MySQL connection object\n");
+    goto cleanup;
+  }
+
+  if (!query)
+  {
+    fprintf(stderr, "Failed to allocate query buffer\n");
+    goto cleanup;
+  }
+
+  if (!mysql_real_connect(conn, "127.0.0.1", "root", "root123", "hashes", 3306, NULL, CLIENT_MULTI_STATEMENTS))
+  {
+    fprintf(stderr, "Connection to database failed: %s\n", mysql_error(conn));
+    goto cleanup;
+  }
+
+  exitCode = mysql_query(conn, "DROP TABLE IF EXISTS tb_c_bulk; CREATE TABLE tb_c_bulk "
+                               "(id INTEGER AUTO_INCREMENT PRIMARY KEY, hash INTEGER NOT NULL)");
+  if (exitCode)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    goto cleanup;
+  }
+  clear_mysql_results(conn);
+
+  buffer += snprintf(query, QUERY_SIZE, "INSERT INTO tb_c_bulk(hash) VALUES ");
+
+  for (i = 1; i <= N; i++)
+  {
+    hash = hash32(i);
+    buffer += snprintf(
+        (char *)(query + buffer),
+        QUERY_SIZE - buffer,
+        "(%d),",
+        hash);
+  }
+
+  query[buffer - 1] = '\0';
+
+  exitCode = mysql_query(conn, "BEGIN; ");
+  if (exitCode)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    goto cleanup;
+  }
+  clear_mysql_results(conn);
+
+  exitCode = mysql_query(conn, query);
+  if (exitCode)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    goto cleanup;
+  }
+  clear_mysql_results(conn);
+
+  exitCode = mysql_query(conn, "COMMIT; ");
+  if (exitCode)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    goto cleanup;
+  }
+  clear_mysql_results(conn);
+
+  exitCode = 0;
+
+cleanup:
+  free(query);
+  if (conn)
+    mysql_close(conn);
+
+  mysql_library_end();
+
+  return exitCode;
+}
