0a38b0c196c6d802e1baa67fedb066940f50c478
amartinsmg <andremindurimg@gmail.com>
2026-03-11 22:16:41 -0300

    feat(sqlite): Implement hash generation
    - Replaced Gaussian CDF with hash generation
    - Updated database connection and table schema

--- implementations/c/src/sqlite_serial.c
+++ implementations/c/src/sqlite_serial.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <libgen.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <sqlite3.h>
-#include "gaussian.h"
+#include <hash.h>
+
+#define N 10000
 
 int main(int argc, char **argv)
 {
   sqlite3 *conn = NULL;
-  int exitCode = -1, i;
-  double x, prob;
-  char dbPath[300],
-      query[90],
+  int32_t i, hash;
+  int exitCode = -1;
+  char query[90],
+      *dbPath = "database.db",
       *errMsg = NULL;
-  sprintf(dbPath, "%s/../sqlite/database.db", argc ? dirname(argv[0]) : ".");
+
   exitCode = sqlite3_open(dbPath, &conn);
   if (exitCode)
   {
     fprintf(stderr, "Connection to database failed\n");
     goto cleanup;
   }
-  exitCode = sqlite3_exec(conn, "DROP TABLE IF EXISTS tb_c_serial; CREATE TABLE tb_c_serial(id INTEGER PRIMARY KEY "
-                              "AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)",
+
+  exitCode = sqlite3_exec(conn, "DROP TABLE IF EXISTS tb_c_serial; CREATE TABLE tb_c_serial(id "
+                                "INTEGER PRIMARY KEY AUTOINCREMENT, hash INTEGER NOT NULL)",
                           0, 0, &errMsg);
   if (exitCode)
   {
@@ -29,11 +33,12 @@ int main(int argc, char **argv)
   }
   sqlite3_free(errMsg);
   errMsg = NULL;
-  for (i = -500; i <= 500; i++)
+
+  for (i = 0; i <= N; i++)
   {
-    x = (double)i / 100.0;
-    prob = gaussianCDF(0, 1, x);
-    sprintf(query, "INSERT INTO tb_c_serial(z_score, cumulative_distribution) VALUES (%.2f, %f)", x, prob);
+    hash = hash32(i);
+    snprintf(query, 90, "INSERT INTO tb_c_serial(hash) VALUES (%d);", hash);
+
     exitCode = sqlite3_exec(conn, query, 0, 0, &errMsg);
     if (exitCode)
     {
@@ -43,11 +48,13 @@ int main(int argc, char **argv)
     sqlite3_free(errMsg);
     errMsg = NULL;
   }
+
   exitCode = 0;
 
 cleanup:
   sqlite3_free(errMsg);
-  if (conn != NULL)
+  if (conn)
     sqlite3_close(conn);
+
   return exitCode;
 }
