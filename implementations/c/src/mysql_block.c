acba1f71d29ec3c861d3a922b01c6810efacb9f8
amartinsmg <andremindurimg@gmail.com>
2026-03-12 12:13:24 -0300

    feat(mysql): store hashes instead of gaussian CDF
    - Implement bulk inserts using transactions
    - Generate and store 32-bit hash values
    - Remove gaussian CDF calculations
    - Increase query buffer size

--- implementations/c/src/mysql_block.c
+++ implementations/c/src/mysql_block.c
@@ -1,54 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <mysql.h>
-#include "gaussian.h"
+#include <hash.h>
+
+#define N 10000
+#define QUERY_SIZE 600000
+
+void clear_mysql_results(MYSQL *conn)
+{
+  MYSQL_RES *res;
+  while (mysql_next_result(conn) == 0)
+  {
+    res = mysql_store_result(conn);
+    if (res)
+      mysql_free_result(res);
+  }
+}
 
 int main(int argc, char **argv)
 {
   MYSQL *conn = mysql_init(NULL);
-  int exitCode = -1, i, buffer = 0;
+  int32_t i, hash;
+  int exitCode = -1,
+      buffer = 0;
   double x, prob;
-  char *query = (char *)malloc(90000 * sizeof(*query));
-  if (conn == NULL)
+  char *query = malloc(QUERY_SIZE * sizeof(*query));
+
+  if (!conn)
   {
     fprintf(stderr, "Failed to initialize MySQL connection object\n");
     goto cleanup;
   }
-  if (query == NULL)
+
+  if (!query)
   {
     fprintf(stderr, "Failed to allocate query buffer\n");
     goto cleanup;
   }
-  if (mysql_real_connect(conn, "localhost", "root", "root123", "gaussian", 3306, NULL, CLIENT_MULTI_STATEMENTS) == NULL)
+
+  if (!mysql_real_connect(conn, "127.0.0.1", "root", "root123", "hashes", 3306, NULL, CLIENT_MULTI_STATEMENTS))
   {
     fprintf(stderr, "Connection to database failed: %s\n", mysql_error(conn));
     goto cleanup;
   }
-  exitCode = mysql_query(conn, "DROP TABLE IF EXISTS tb_c_block; CREATE TABLE tb_c_block(id INTEGER AUTO_INCREMENT "
-                               "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
+
+  exitCode = mysql_query(conn, "DROP TABLE IF EXISTS tb_c_batch; CREATE TABLE tb_c_batch "
+                             "(id INTEGER AUTO_INCREMENT PRIMARY KEY, hash INTEGER NOT NULL)");
   if (exitCode)
   {
     fprintf(stderr, "%s\n", mysql_error(conn));
     goto cleanup;
   }
-  mysql_next_result(conn);
-  for (i = -500; i <= 500; i++)
+  clear_mysql_results(conn);
+
+  for (i = 1; i <= N; i++)
   {
-    x = (double)i / 100.0;
-    prob = gaussianCDF(0, 1, x);
-    buffer += sprintf((char *)(query + buffer), "INSERT INTO tb_c_block(z_score, cumulative_distribution) VALUES (%.2f, %f);", x, prob);
+    hash = hash32(i);
+    buffer += snprintf(
+        (char *)(query + buffer),
+        QUERY_SIZE - buffer,
+        "INSERT INTO tb_c_batch(hash) VALUES (%d);",
+        hash);
   }
+
+  exitCode = mysql_query(conn, "BEGIN; ");
+  if (exitCode)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    goto cleanup;
+  }
+  clear_mysql_results(conn);
+
   exitCode = mysql_query(conn, query);
   if (exitCode)
   {
     fprintf(stderr, "%s\n", mysql_error(conn));
     goto cleanup;
   }
+  clear_mysql_results(conn);
+
+  exitCode = mysql_query(conn, "COMMIT; ");
+  if (exitCode)
+  {
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    goto cleanup;
+  }
+  clear_mysql_results(conn);
+
   exitCode = 0;
 
 cleanup:
   free(query);
-  if (conn != NULL)
+  if (conn)
     mysql_close(conn);
+
+  mysql_library_end();
+
   return exitCode;
 }
