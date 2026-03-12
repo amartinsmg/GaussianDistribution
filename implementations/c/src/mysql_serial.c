33c72cfc387f46363d6560684bcda72afae2733b
amartinsmg <andremindurimg@gmail.com>
2026-03-12 12:14:27 -0300

    feat(mysql): Implement hash generation
    - Replaced Gaussian CDF with hash generation
    - Updated database connection and table schema

--- implementations/c/src/mysql_serial.c
+++ implementations/c/src/mysql_serial.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <mysql.h>
-#include "gaussian.h"
+#include <hash.h>
+
+#define N 10000
 
 int main(int argc, char **argv)
 {
   MYSQL *conn = mysql_init(NULL);
-  int exitCode = -1, i;
-  double x, prob;
+  int32_t i, hash;
+  int exitCode = -1;
   char query[90];
-  if (conn == NULL)
+
+  if (!conn)
   {
     fprintf(stderr, "Failed to initialize MySQL connection object\n");
     goto cleanup;
   }
-  if (mysql_real_connect(conn, "localhost", "root", "root123", "gaussian", 3306, NULL, CLIENT_MULTI_STATEMENTS) == NULL)
+
+  if (!mysql_real_connect(conn, "127.0.0.1", "root", "root123", "hashes", 3306, NULL, CLIENT_MULTI_STATEMENTS))
   {
     fprintf(stderr, "Connection to database failed: %s\n", mysql_error(conn));
     goto cleanup;
   }
-  exitCode = mysql_query(conn, "DROP TABLE IF EXISTS tb_c_serial; CREATE TABLE tb_c_serial(id INTEGER AUTO_INCREMENT "
-                               "PRIMARY KEY, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
+
+  exitCode = mysql_query(conn, "DROP TABLE IF EXISTS tb_c_serial; CREATE TABLE tb_c_serial "
+                             "(id INTEGER AUTO_INCREMENT PRIMARY KEY, hash INTEGER NOT NULL)");
   if (exitCode)
   {
     fprintf(stderr, "%s\n", mysql_error(conn));
     goto cleanup;
   }
   mysql_next_result(conn);
-  for (i = -500; i <= 500; i++)
+
+  for (i = 1; i <= N; i++)
   {
-    x = (double)i / 100.0;
-    prob = gaussianCDF(0, 1, x);
-    sprintf(query, "INSERT INTO tb_c_serial(z_score, cumulative_distribution) VALUES (%.2f, %f)", x, prob);
+    hash = hash32(i);
+    snprintf(query, 90, "INSERT INTO tb_c_serial(hash) VALUES (%d)", hash);
+
     exitCode = mysql_query(conn, query);
     if (exitCode)
     {
@@ -40,10 +47,14 @@ int main(int argc, char **argv)
     }
     mysql_next_result(conn);
   }
+
   exitCode = 0;
 
 cleanup:
-  if (conn != NULL)
+  if (conn)
     mysql_close(conn);
+
+  mysql_library_end();
+
   return exitCode;
 }
