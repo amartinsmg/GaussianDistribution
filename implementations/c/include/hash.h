82dc2292abc9689277cf8f6679711b210727df10
amartinsmg <andremindurimg@gmail.com>
2026-03-11 22:06:12 -0300

    feat(hash): add 32-bit integer hash function

---
+++ implementations/c/include/hash.h
@@ -0,0 +1,16 @@
+#ifndef HASH
+#define HASH
+
+#include <stdint.h>
+
+int32_t hash32(int32_t x)
+{
+  x ^= x >> 16;
+  x *= 0x7feb352d;
+  x ^= x >> 15;
+  x *= 0x846ca68d;
+  x ^= x >> 16;
+  return x & 0xffffffff;
+}
+
+#endif /*HASH*/
