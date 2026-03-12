ce7a1e5bf6e9d87786d9dcb141f1c0a1d30fda49
amartinsmg <andremindurimg@gmail.com>
2026-03-11 23:36:28 -0300

    feat(mysql): increase hash insertion count
    - Changed loop to insert 10000 hashes
    - Updated table creation logic for clarity
    - Increased hash insertion range

--- implementations/java/src/MySQLSerial.java
+++ implementations/java/src/MySQLSerial.java
@@ -7,18 +7,26 @@ public class MySQLSerial {
         String query;
         int i, hash;
         try (
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hashes?allowMultiQueries=true", "root", "root123");
-            Statement stmt = conn.createStatement();
-        ) {
-            stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial; CREATE TABLE tb_java_serial(id INTEGER AUTO_INCREMENT "
-                    + "PRIMARY KEY, hash INTEGER NOT NULL)");
-            for (i = 0; i <= 1000; i++) {
-                hash = Hash.hash32(i);
-                query = String.format("INSERT INTO tb_java_serial(hash) VALUES (%d)", hash);
-                stmt.executeUpdate(query);
-            }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
+		Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://127.0.0.1:3306/hashes?allowMultiQueries=true",
+				"root",
+				"root123"
+		);
+		Statement stmt = conn.createStatement();
+		) {
+			stmt.executeUpdate("DROP TABLE IF EXISTS tb_java_serial;"
+					+ "CREATE TABLE tb_java_serial "
+					+ "(id INTEGER AUTO_INCREMENT PRIMARY KEY, "
+					+ "hash INTEGER NOT NULL)");
+
+			for (i = 1; i <= 10000; i++) {
+				hash = Hash.hash32(i);
+				query = String.format("INSERT INTO tb_java_serial(hash) VALUES (%d)", hash);
+				stmt.executeUpdate(query);
+			}
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
     }
 }
