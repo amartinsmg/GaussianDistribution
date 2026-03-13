9db975e4e2648dba53cdc40fa3bb71348ac0a8a9
amartinsmg <andremindurimg@gmail.com>
2026-03-12 23:46:20 -0300

    refactor(mysql-serial): store hashes instead of Gaussian CDF
    - Switched from Gaussian CDF to hash32 calc
    - Updated table schema to store only hashes
    - Implemented with statement

--- implementations/php/src/postgres_serial.php
+++ implementations/php/src/postgres_serial.php
@@ -1,17 +1,26 @@
 <?php
 
-include "gaussian.php";
+include "hashes.php";
 
 try {
-  $conn = new PDO("pgsql:host=localhost;port=5432;dbname=gaussian", "root", "root123");
-  $conn->exec("DROP TABLE IF EXISTS tb_php_serial; CREATE TABLE tb_php_serial(id SERIAL PRIMARY KEY, 
-  z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL)");
-  for ($i = -500; $i <= 500; $i++) {
-    $x = $i / 100;
-    $prob = gaussianCDF(0, 1, $x);
-    $query = sprintf("INSERT INTO tb_php_serial(z_score, cumulative_distribution) VALUES (%.2f, %f);", $x, $prob);
-    $conn->exec($query);
-  }
+    $conn = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=hashes", "root", "root123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conn->exec(<<<HEREA
+            DROP TABLE IF EXISTS tb_php_serial;
+            CREATE TABLE tb_php_serial
+            (id SERIAL PRIMARY KEY,
+            hash INTEGER NULL);
+            HEREA);
+
+    for ($i = 1; $i <= 10000; $i++) {
+        $hash = hash32($i);
+        $query = sprintf("INSERT INTO tb_php_serial(hash) VALUES (%d);", $hash);
+        $conn->query($query);
+    }
 } catch (\Throwable $th) {
-  echo $th->getMessage();
+    if ($conn->inTransaction()) $conn->rollBack();
+    echo $th->getMessage();
+} finally {
+    $conn = null;
 }
