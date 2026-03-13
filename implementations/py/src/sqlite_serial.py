b82e0ccecff980ef1fef656c0d01977fb694a764
amartinsmg <andremindurimg@gmail.com>
2026-03-12 21:24:04 -0300

    refactor(sqlite-serial): store hashes instead of Gaussian CDF
    - Switched from Gaussian CDF to hash32 calc
    - Updated table schema to store only hashes
    - Implemented with statement

--- implementations/py/src/sqlite_serial.py
+++ implementations/py/src/sqlite_serial.py
@@ -1,21 +1,26 @@
-from gaussian import gaussianCDF
-from os.path import dirname
+from hashes import hash32
+from pathlib import Path
 from sqlite3 import connect
 
-dbPath = dirname(__file__) + '/../../sqlite/database.db'
+dbPath = Path(__file__).parent.parent / "database.db"
 
 try:
-	conn = connect(dbPath)
-	cur = conn.cursor()
-	cur.executescript('''DROP TABLE IF EXISTS tb_py_serial; CREATE TABLE tb_py_serial(id INTEGER PRIMARY KEY
-	AUTOINCREMENT, z_score REAL NOT NULL, cumulative_distribution REAL NOT NULL);''')
-	conn.commit()
-	for i in range(-500, 501):
-		x = i / 100
-		prob = gaussianCDF(0, 1, x)
-		cur.execute(
-			f'INSERT INTO tb_py_serial(z_score, cumulative_distribution) VALUES ({x:.2f}, {prob:.6f})')
-		conn.commit()
-	conn.close()
+    conn = connect(dbPath, autocommit=True)
+    cur = conn.cursor()
+    cur.executescript(
+        """DROP TABLE IF EXISTS tb_py_serial;
+                CREATE TABLE tb_py_serial
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                hash INTEGER NOT NULL);"""
+    )
+
+    for i in range(1, 10_001):
+        hash = hash32(i)
+        cur.execute("INSERT INTO tb_py_serial(hash) VALUES (?)", (hash,))
+
 except Exception as e:
-	print(e)
+    print(e)
+
+finally:
+    if conn:
+        conn.close()
